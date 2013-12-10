def46bbfd1420457a8c74c50f529d60ee891a3b4
Dan LaRocque <dalaro@hopcount.org>
2013-12-10 08:36:14 -0500

    Avoid serializing Guava Lists.transform() closure
    HintedTitanGraph is serializable.  It assigned the return value of
    Guava's Lists.transform(...) to a pair of final fields.  But the
    returned list retained a reference to a Function, which in turn had
    reference to a KeyRange.  KeyRange is not serializable, and all of
    these retained references are implementation details that really
    shouldn't be serialized in the first place.  Now HintedTitanGraph
    takes care to eagerly evaluate the transform and store the result in a
    standard-library ArrayList.

--- titan-rexster/src/main/java/com/thinkaurelius/titan/tinkerpop/rexster/HintedTitanGraph.java
+++ titan-rexster/src/main/java/com/thinkaurelius/titan/tinkerpop/rexster/HintedTitanGraph.java
@@ -1,8 +1,8 @@
 package com.thinkaurelius.titan.tinkerpop.rexster;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 import com.thinkaurelius.titan.diskstorage.StaticBuffer;
 import com.thinkaurelius.titan.diskstorage.keycolumnvalue.KeyRange;
@@ -43,7 +43,28 @@ public class HintedTitanGraph implements HintedGraph<StaticBuffer> {
     }
     
     private <E extends Element> List<ElementRange<StaticBuffer, E>> keyRangeToElementRange(final List<KeyRange> local, final Class<E> token) {
-        
-        return Lists.transform(local, new KeyRangeToElementRange<E>(token));
+
+        /*
+         * Don't return Lists.transform(...) here.
+         *
+         * The object returned by Lists.transform(...) retains a reference to
+         * the KeyRange named local. This reference is not transient. Java
+         * serialization will die on this reference when attempting to send our
+         * edge/vertex ranges over JGroups because KeyRange is not
+         * serializable.
+         *
+         * We could make KeyRange serializable. However, it doesn't really need
+         * to be serializable. It's an implementation detail that is totally
+         * irrelevant from the perspective of a HintedGraph interface user.
+         *
+         * This is why we go out of our way to return a standard-library
+         * ArrayList instead of just returning Lists.transform(...).
+         */
+        //return Lists.transform(local, new KeyRangeToElementRange<E>(token));
+
+        ArrayList<ElementRange<StaticBuffer, E>> result =
+                new ArrayList<ElementRange<StaticBuffer, E>>(local.size());
+        result.addAll(Lists.transform(local, new KeyRangeToElementRange<E>(token)));
+        return result;
     }
 }
