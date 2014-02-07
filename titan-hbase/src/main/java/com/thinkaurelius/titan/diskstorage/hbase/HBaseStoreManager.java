48cfc19c99a7cdda33fa63c526c9b51ddfd9fe2b
Dan LaRocque <dalaro@hopcount.org>
2014-02-07 18:54:47 -0500

    Splitting HBase into modules for 0.94 and 0.96
    This commit compiles and passes tests against HBase 0.94.16 + Hadoop
    1.0.4 and against HBase 0.96.1.1-hadoop2 + Hadoop 2.2.0.
    
    Several things still need work: titan-dist's dependency on titan-hbase
    is no longer complete and effective since that's just a pom, some of
    the hadoop dependencies need to be made optional and/or provided/test
    scoped, probably some other stuff...

--- titan-hbase/src/main/java/com/thinkaurelius/titan/diskstorage/hbase/HBaseStoreManager.java
+++ titan-hbase/src/main/java/com/thinkaurelius/titan/diskstorage/hbase/HBaseStoreManager.java
@@ -20,7 +20,6 @@ import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableNotFoundException;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.io.hfile.Compression;
 import org.apache.hadoop.hbase.util.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -269,7 +268,9 @@ public class HBaseStoreManager extends DistributedStoreManager implements KeyCol
         if (cf == null) {
             try {
                 adm.disableTable(tableName);
-                desc.addFamily(new HColumnDescriptor(columnFamily).setCompressionType(Compression.Algorithm.GZ));
+                HColumnDescriptor cdesc = new HColumnDescriptor(columnFamily);
+                HBaseSupport.setCompression(cdesc, "GZ");
+                desc.addFamily(cdesc);
                 adm.modifyTable(tableName.getBytes(), desc);
 
                 try {
@@ -287,18 +288,6 @@ public class HBaseStoreManager extends DistributedStoreManager implements KeyCol
                 logger.debug("Swallowing exception {}", ee);
             } catch (IOException ee) {
                 throw new TemporaryStorageException(ee);
-            }
-        } else { // check if compression was enabled, if not - enable it
-            if (cf.getCompressionType() == null || cf.getCompressionType() == Compression.Algorithm.NONE) {
-                try {
-                    adm.disableTable(tableName);
-
-                    adm.modifyColumn(tableName, cf.setCompressionType(Compression.Algorithm.GZ));
-
-                    adm.enableTable(tableName);
-                } catch (IOException e) {
-                    throw new TemporaryStorageException(e);
-                }
             }
         }
     }
