d71100e92d1a2879d0a63409b40273b28e24a2ee
Dan LaRocque <dalaro@hopcount.org>
2014-10-15 10:54:36 -0400

    Catch Throwable in ReflectiveConfigOptionLoader
    For #785

--- titan-core/src/main/java/com/thinkaurelius/titan/core/util/ReflectiveConfigOptionLoader.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/core/util/ReflectiveConfigOptionLoader.java
@@ -134,7 +134,7 @@ public enum ReflectiveConfigOptionLoader {
                     Class.forName(c, true, cl);
                     loadedClasses++;
                     log.debug("Loaded class {} with selected loader {}", c, cl);
-                } catch (ClassNotFoundException e) {
+                } catch (Throwable e) {
                     log.debug("Unable to load class {} with selected loader {}", c, cl, e);
                 }
             } else {
@@ -147,7 +147,7 @@ public enum ReflectiveConfigOptionLoader {
                         log.debug("Located functioning classloader {}; using it for remaining classload attempts", cl);
                         foundLoader = true;
                         break;
-                    } catch (ClassNotFoundException e) {
+                    } catch (Throwable e) {
                         log.debug("Unable to load class {} with loader {}", c, cl, e);
                     }
                 }
