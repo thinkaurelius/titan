a6e6d75845aace871402594ae7a851eee9f7ce3a
Dan LaRocque <dalaro@hopcount.org>
2014-11-19 08:48:12 -0500

    Delete Cassandra data between Expect ITs
    8fa00662730ec74089dc4dd604cb247f4dadafa9 introduced a new test,
    forming a pair of Cassandra tests that could interfere with each other
    by using the same shared embedded Cassandra instance without dropping
    data between methods.  This adds a @Before that drops those data.

--- titan-dist/src/test/java/com/thinkaurelius/titan/pkgtest/FaunusCassandraIT.java
+++ titan-dist/src/test/java/com/thinkaurelius/titan/pkgtest/FaunusCassandraIT.java
@@ -1,5 +1,9 @@
 package com.thinkaurelius.titan.pkgtest;
 
+import com.thinkaurelius.titan.core.TitanFactory;
+import com.thinkaurelius.titan.diskstorage.BackendException;
+import com.thinkaurelius.titan.graphdb.database.StandardTitanGraph;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -14,6 +18,13 @@ public class FaunusCassandraIT extends AbstractTitanAssemblyIT {
     @BeforeClass
     public static void startCassandra() {
         CassandraStorageSetup.startCleanEmbedded();
+    }
+
+    @Before
+    public void deleteCassandraData() throws BackendException {
+        StandardTitanGraph g = (StandardTitanGraph)TitanFactory.open("cassandrathrift:127.0.0.1");
+        g.getBackend().clearStorage();
+        // shutdown not necessary
     }
 
     @Test
