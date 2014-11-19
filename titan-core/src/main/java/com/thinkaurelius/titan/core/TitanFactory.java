thinkaurelius/titan

commit a2149cec1d2d9b52b1bad596ef8cb1accdd2f3db

Breaking static initializer cycles
GraphDatabaseConfiguration no longer contains references to Backend.
Those references have been replaced by two new self-contained enums
representing storage managers and index backends.  Fixes #831.

titan-core/src/main/java/com/thinkaurelius/titan/core/TitanFactory.java
@@ -5,8 +5,8 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.Iterators;
 import com.thinkaurelius.titan.core.log.LogProcessorFramework;
 import com.thinkaurelius.titan.core.log.TransactionRecovery;
-import com.thinkaurelius.titan.core.util.ReflectiveConfigOptionLoader;
 import com.thinkaurelius.titan.diskstorage.Backend;
+import com.thinkaurelius.titan.diskstorage.StandardStoreManager;
 import com.thinkaurelius.titan.diskstorage.configuration.*;
 import com.thinkaurelius.titan.diskstorage.configuration.backend.CommonsConfiguration;
 import com.thinkaurelius.titan.diskstorage.util.time.StandardTimestamp;
@@ -170,13 +170,13 @@ public class TitanFactory {
             int pos = shortcutOrFile.indexOf(':');
             if (pos<0) pos = shortcutOrFile.length();
             String backend = shortcutOrFile.substring(0,pos);
-            Preconditions.checkArgument(Backend.REGISTERED_STORAGE_MANAGERS_SHORTHAND.containsKey(backend.toLowerCase()), "Backend shorthand unknown: %s", backend);
+            Preconditions.checkArgument(StandardStoreManager.getAllManagerClasses().containsKey(backend.toLowerCase()), "Backend shorthand unknown: %s", backend);
             String secondArg = null;
             if (pos+1<shortcutOrFile.length()) secondArg = shortcutOrFile.substring(pos + 1).trim();
             BaseConfiguration config = new BaseConfiguration();
             ModifiableConfiguration writeConfig = new ModifiableConfiguration(ROOT_NS,new CommonsConfiguration(config), BasicConfiguration.Restriction.NONE);
             writeConfig.set(STORAGE_BACKEND,backend);
-            ConfigOption option = Backend.REGISTERED_STORAGE_MANAGERS_SHORTHAND.get(backend.toLowerCase());
+            ConfigOption option = Backend.getStoreShorthandOptions().get(backend.toLowerCase());
             if (option==null) {
                 Preconditions.checkArgument(secondArg==null);
             } else if (option==STORAGE_DIRECTORY || option==STORAGE_CONF_FILE) {

titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/Backend.java
@@ -364,7 +364,7 @@ public class Backend implements LockerProvider {
 
     public static KeyColumnValueStoreManager getStorageManager(Configuration storageConfig) {
         StoreManager manager = getImplementationClass(storageConfig, storageConfig.get(STORAGE_BACKEND),
-                REGISTERED_STORAGE_MANAGERS);
+                StandardStoreManager.getAllManagerClasses());
         if (manager instanceof OrderedKeyValueStoreManager) {
             manager = new OrderedKeyValueStoreManagerAdapter((OrderedKeyValueStoreManager) manager, STATIC_KEY_LENGTHS);
         }
@@ -410,7 +410,7 @@ public class Backend implements LockerProvider {
             Preconditions.checkArgument(StringUtils.isNotBlank(index), "Invalid index name [%s]", index);
             log.info("Configuring index [{}]", index);
             IndexProvider provider = getImplementationClass(config.restrictTo(index), config.get(INDEX_BACKEND,index),
-                    REGISTERED_INDEX_PROVIDERS);
+                    StandardIndexProvider.getAllProviderClasses());
             Preconditions.checkNotNull(provider);
             builder.put(index, provider);
         }
@@ -536,44 +536,25 @@ public class Backend implements LockerProvider {
 
     //############ Registered Storage Managers ##############
 
-    private static final ImmutableMap<String, String> REGISTERED_STORAGE_MANAGERS;
+    private static final ImmutableMap<StandardStoreManager, ConfigOption<?>> STORE_SHORTHAND_OPTIONS;
 
     static {
-        ImmutableMap.Builder<String, String> b = ImmutableMap.builder();
-        b.put("berkeleyje", "com.thinkaurelius.titan.diskstorage.berkeleyje.BerkeleyJEStoreManager");
-        b.put("infinispan", "com.thinkaurelius.titan.diskstorage.infinispan.InfinispanCacheStoreManager");
-        b.put("cassandrathrift", "com.thinkaurelius.titan.diskstorage.cassandra.thrift.CassandraThriftStoreManager");
-        b.put("cassandra", "com.thinkaurelius.titan.diskstorage.cassandra.astyanax.AstyanaxStoreManager");
-        b.put("astyanax", "com.thinkaurelius.titan.diskstorage.cassandra.astyanax.AstyanaxStoreManager");
-        b.put("hbase", "com.thinkaurelius.titan.diskstorage.hbase.HBaseStoreManager");
-        b.put("embeddedcassandra", "com.thinkaurelius.titan.diskstorage.cassandra.embedded.CassandraEmbeddedStoreManager");
-        b.put("inmemory", "com.thinkaurelius.titan.diskstorage.keycolumnvalue.inmemory.InMemoryStoreManager");
-        REGISTERED_STORAGE_MANAGERS = b.build();
+        EnumMap<StandardStoreManager, ConfigOption<?>> m =
+                new EnumMap<StandardStoreManager, ConfigOption<?>>(StandardStoreManager.class);
+
+        m.put(StandardStoreManager.BDB_JE, STORAGE_DIRECTORY);
+        m.put(StandardStoreManager.CASSANDRA_ASTYANAX, STORAGE_HOSTS);
+        m.put(StandardStoreManager.CASSANDRA_EMBEDDED, STORAGE_CONF_FILE);
+        m.put(StandardStoreManager.CASSANDRA_THRIFT, STORAGE_HOSTS);
+        m.put(StandardStoreManager.HBASE, STORAGE_HOSTS);
+        //m.put(StandardStorageBackend.IN_MEMORY, null);
+
+        STORE_SHORTHAND_OPTIONS = Maps.immutableEnumMap(m);
     }
 
-    public static final Map<String, String> getRegisteredStoreManagers() {
-        return REGISTERED_STORAGE_MANAGERS;
+    public static ImmutableMap<StandardStoreManager, ConfigOption<?>> getStoreShorthandOptions() {
+        return STORE_SHORTHAND_OPTIONS;
     }
-
-    public static final Map<String, ConfigOption> REGISTERED_STORAGE_MANAGERS_SHORTHAND = new HashMap<String, ConfigOption>() {{
-        put("berkeleyje", STORAGE_DIRECTORY);
-        put("hazelcast", STORAGE_DIRECTORY);
-        put("hazelcastcache", STORAGE_DIRECTORY);
-        put("infinispan", STORAGE_DIRECTORY);
-        put("cassandra", STORAGE_HOSTS);
-        put("cassandrathrift", STORAGE_HOSTS);
-        put("astyanax", STORAGE_HOSTS);
-        put("hbase", STORAGE_HOSTS);
-        put("embeddedcassandra", STORAGE_CONF_FILE);
-        put("inmemory", null);
-    }};
-
-    public static final Map<String, String> REGISTERED_INDEX_PROVIDERS = new HashMap<String, String>() {{
-        put("lucene", "com.thinkaurelius.titan.diskstorage.lucene.LuceneIndex");
-        put("elasticsearch", "com.thinkaurelius.titan.diskstorage.es.ElasticSearchIndex");
-        put("es", "com.thinkaurelius.titan.diskstorage.es.ElasticSearchIndex");
-        put("solr", "com.thinkaurelius.titan.diskstorage.solr.SolrIndex");
-    }};
 
     public static final Map<String,String> REGISTERED_LOG_MANAGERS = new HashMap<String, String>() {{
         put("default","com.thinkaurelius.titan.diskstorage.log.kcvs.KCVSLogManager");
@@ -669,8 +650,8 @@ public class Backend implements LockerProvider {
         } catch (IOException e) {
             throw new AssertionError(e);
         }
-        registerShorthands(props, "storage.", REGISTERED_STORAGE_MANAGERS);
-        registerShorthands(props, "index.", REGISTERED_INDEX_PROVIDERS);
+        registerShorthands(props, "storage.", StandardStoreManager.getAllManagerClasses());
+        registerShorthands(props, "index.", StandardIndexProvider.getAllProviderClasses());
     }
 
     public static final void registerShorthands(Properties props, String prefix, Map<String, String> shorthands) {

titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/StandardStoreManager.java
@@ -1,0 +1,64 @@
+package com.thinkaurelius.titan.diskstorage;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.*;
+
+/**
+ * This enum is only intended for use by Titan internals.
+ * It is subject to backwards-incompatible change.
+ */
+public enum StandardStoreManager {
+    BDB_JE("com.thinkaurelius.titan.diskstorage.berkeleyje.BerkeleyJEStoreManager", "berkeleyje"),
+    CASSANDRA_THRIFT("com.thinkaurelius.titan.diskstorage.cassandra.thrift.CassandraThriftStoreManager", "cassandrathrift"),
+    CASSANDRA_ASTYANAX("com.thinkaurelius.titan.diskstorage.cassandra.astyanax.AstyanaxStoreManager", ImmutableList.of("cassandra", "astyanax")),
+    CASSANDRA_EMBEDDED("com.thinkaurelius.titan.diskstorage.cassandra.embedded.CassandraEmbeddedStoreManager", "embeddedcassandra"),
+    HBASE("com.thinkaurelius.titan.diskstorage.hbase.HBaseStoreManager", "hbase"),
+    IN_MEMORY("com.thinkaurelius.titan.diskstorage.keycolumnvalue.inmemory.InMemoryStoreManager", "inmemory");
+
+    private final String managerClass;
+    private final ImmutableList<String> shorthands;
+
+    StandardStoreManager(String managerClass, ImmutableList<String> shorthands) {
+        this.managerClass = managerClass;
+        this.shorthands = shorthands;
+    }
+
+    StandardStoreManager(String managerClass, String shorthand) {
+        this(managerClass, ImmutableList.of(shorthand));
+    }
+
+    public List<String> getShorthands() {
+        return shorthands;
+    }
+
+    public String getManagerClass() {
+        return managerClass;
+    }
+
+    private static final ImmutableList<String> ALL_SHORTHANDS;
+    private static final ImmutableMap<String, String> ALL_MANAGER_CLASSES;
+
+    static {
+        StandardStoreManager backends[] = values();
+        List<String> tempShorthands = new ArrayList<String>();
+        Map<String, String> tempClassMap = new HashMap<String, String>();
+        for (int i = 0; i < backends.length; i++) {
+            tempShorthands.addAll(backends[i].getShorthands());
+            for (String shorthand : backends[i].getShorthands()) {
+                tempClassMap.put(shorthand, backends[i].getManagerClass());
+            }
+        }
+        ALL_SHORTHANDS = ImmutableList.copyOf(tempShorthands);
+        ALL_MANAGER_CLASSES = ImmutableMap.copyOf(tempClassMap);
+    }
+
+    public static final List<String> getAllShorthands() {
+        return ALL_SHORTHANDS;
+    }
+
+    public static final Map<String, String> getAllManagerClasses() {
+        return ALL_MANAGER_CLASSES;
+    }
+}

titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/StandardIndexProvider.java
@@ -1,0 +1,64 @@
+package com.thinkaurelius.titan.diskstorage;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This enum is only intended for use by Titan internals.
+ * It is subject to backwards-incompatible change.
+ */
+public enum StandardIndexProvider {
+    LUCENE("com.thinkaurelius.titan.diskstorage.lucene.LuceneIndex", "lucene"),
+    ELASTICSEARCH("com.thinkaurelius.titan.diskstorage.es.ElasticSearchIndex", ImmutableList.of("elasticsearch", "es")),
+    SOLR("com.thinkaurelius.titan.diskstorage.solr.SolrIndex", "solr");
+
+    private final String providerName;
+    private final ImmutableList<String> shorthands;
+
+    StandardIndexProvider(String providerName, String shorthand) {
+        this(providerName, ImmutableList.of(shorthand));
+    }
+
+    StandardIndexProvider(String providerName, ImmutableList<String> shorthands) {
+        this.providerName = providerName;
+        this.shorthands = shorthands;
+    }
+
+    private static final ImmutableList<String> ALL_SHORTHANDS;
+    private static final ImmutableMap<String, String> ALL_MANAGER_CLASSES;
+
+    public List<String> getShorthands() {
+        return shorthands;
+    }
+
+    public String getProviderName() {
+        return providerName;
+    }
+
+    static {
+        StandardIndexProvider backends[] = values();
+        List<String> tempShorthands = new ArrayList<String>();
+        Map<String, String> tempClassMap = new HashMap<String, String>();
+        for (int i = 0; i < backends.length; i++) {
+            tempShorthands.addAll(backends[i].getShorthands());
+            for (String shorthand : backends[i].getShorthands()) {
+                tempClassMap.put(shorthand, backends[i].getProviderName());
+            }
+        }
+        ALL_SHORTHANDS = ImmutableList.copyOf(tempShorthands);
+        ALL_MANAGER_CLASSES = ImmutableMap.copyOf(tempClassMap);
+    }
+
+    public static final List<String> getAllShorthands() {
+        return ALL_SHORTHANDS;
+    }
+
+    public static final Map<String, String> getAllProviderClasses() {
+        return ALL_MANAGER_CLASSES;
+    }
+}

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/configuration/GraphDatabaseConfiguration.java
@@ -7,6 +7,8 @@ import com.thinkaurelius.titan.core.*;
 import com.thinkaurelius.titan.core.attribute.AttributeHandler;
 import com.thinkaurelius.titan.core.attribute.Duration;
 import com.thinkaurelius.titan.core.schema.DefaultSchemaMaker;
+import com.thinkaurelius.titan.diskstorage.StandardIndexProvider;
+import com.thinkaurelius.titan.diskstorage.StandardStoreManager;
 import com.thinkaurelius.titan.diskstorage.keycolumnvalue.ttl.TTLKVCSManager;
 import com.thinkaurelius.titan.graphdb.blueprints.BlueprintsDefaultSchemaMaker;
 import com.thinkaurelius.titan.graphdb.database.management.ManagementSystem;
@@ -340,7 +342,7 @@ public class GraphDatabaseConfiguration {
     public static final ConfigOption<String> STORAGE_BACKEND = new ConfigOption<String>(STORAGE_NS,"backend",
             "The primary persistence provider used by Titan.  This is required.  It should be set one of " +
             "Titan's built-in shorthand names for its standard storage backends " +
-            "(shorthands: " + Joiner.on(", ").join(Backend.getRegisteredStoreManagers().keySet()) + ") " +
+            "(shorthands: " + Joiner.on(", ").join(StandardStoreManager.getAllShorthands()) + ") " +
             "or to the full package and classname of a custom/third-party StoreManager implementation.",
             ConfigOption.Type.LOCAL, String.class);
 //    public static final String STORAGE_BACKEND_KEY = "backend";
@@ -770,7 +772,7 @@ public class GraphDatabaseConfiguration {
             "\"" + INDEX_NS.getName() + "\" and \"backend\" is unique among appearances." +
             "Similar to the storage backend, this should be set to one of " +
             "Titan's built-in shorthand names for its standard index backends " +
-            "(shorthands: " + Joiner.on(", ").join(Backend.REGISTERED_INDEX_PROVIDERS.keySet()) + ") " +
+            "(shorthands: " + Joiner.on(", ").join(StandardIndexProvider.getAllShorthands()) + ") " +
             "or to the full package and classname of a custom/third-party IndexProvider implementation.",
             ConfigOption.Type.GLOBAL_OFFLINE, "elasticsearch");
 //    public static final String INDEX_BACKEND_KEY = "backend";

titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TestMockIndexProvider.java
@@ -32,7 +32,7 @@ public class TestMockIndexProvider implements IndexProvider {
 
     public TestMockIndexProvider(Configuration config) {
         this.index = Backend.getImplementationClass(config, config.get(INDEX_BACKEND_PROXY),
-                Backend.REGISTERED_INDEX_PROVIDERS);
+                StandardIndexProvider.getAllProviderClasses());
         this.failAdds = config.get(INDEX_MOCK_FAILADD);
     }
 

commit 8b0a078edf50ba93c88c6be6ab3220abaf49ea59

Fix bug in shorthand argument interpretation
a2149cec1d2d9b52b1bad596ef8cb1accdd2f3db broke a static initializer
cycle triggered by TitanFactory.open('backend:arg') shorthand
invocation, but it also added a bug that broke interpretation the
'arg' part of the shorthand.  This is related to #831.

titan-core/src/main/java/com/thinkaurelius/titan/core/TitanFactory.java
@@ -176,7 +176,7 @@ public class TitanFactory {
             BaseConfiguration config = new BaseConfiguration();
             ModifiableConfiguration writeConfig = new ModifiableConfiguration(ROOT_NS,new CommonsConfiguration(config), BasicConfiguration.Restriction.NONE);
             writeConfig.set(STORAGE_BACKEND,backend);
-            ConfigOption option = Backend.getStoreShorthandOptions().get(backend.toLowerCase());
+            ConfigOption option = Backend.getOptionForShorthand(backend);
             if (option==null) {
                 Preconditions.checkArgument(secondArg==null);
             } else if (option==STORAGE_DIRECTORY || option==STORAGE_CONF_FILE) {

titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/Backend.java
@@ -552,8 +552,18 @@ public class Backend implements LockerProvider {
         STORE_SHORTHAND_OPTIONS = Maps.immutableEnumMap(m);
     }
 
-    public static ImmutableMap<StandardStoreManager, ConfigOption<?>> getStoreShorthandOptions() {
-        return STORE_SHORTHAND_OPTIONS;
+    public static ConfigOption<?> getOptionForShorthand(String shorthand) {
+        if (null == shorthand)
+            return null;
+
+        shorthand = shorthand.toLowerCase();
+
+        for (StandardStoreManager m : STORE_SHORTHAND_OPTIONS.keySet()) {
+            if (m.getShorthands().contains(shorthand))
+                return STORE_SHORTHAND_OPTIONS.get(m);
+        }
+
+        return null;
     }
 
     public static final Map<String,String> REGISTERED_LOG_MANAGERS = new HashMap<String, String>() {{

titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/StandardStoreManager.java
@@ -54,11 +54,11 @@ public enum StandardStoreManager {
         ALL_MANAGER_CLASSES = ImmutableMap.copyOf(tempClassMap);
     }
 
-    public static final List<String> getAllShorthands() {
+    public static List<String> getAllShorthands() {
         return ALL_SHORTHANDS;
     }
 
-    public static final Map<String, String> getAllManagerClasses() {
+    public static Map<String, String> getAllManagerClasses() {
         return ALL_MANAGER_CLASSES;
     }
 }