e91119524b87a2d87f4cf23066bbc0866634464e
Dan LaRocque <dalaro@hopcount.org>
2014-12-17 10:37:41 -0500

    Add Cardinality & Multiplicity enum shell imports
    The Cardinality.* and Multiplicity.* enum names are now imported
    automatically in gremlin.sh and rexster-console.sh as distributed with
    Titan.
    
    For #869

--- titan-core/src/main/java/com/thinkaurelius/titan/tinkerpop/gremlin/Imports.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/tinkerpop/gremlin/Imports.java
@@ -1,5 +1,7 @@
 package com.thinkaurelius.titan.tinkerpop.gremlin;
 
+import com.thinkaurelius.titan.core.Cardinality;
+import com.thinkaurelius.titan.core.Multiplicity;
 import com.thinkaurelius.titan.core.attribute.Geo;
 import com.thinkaurelius.titan.core.attribute.Text;
 import com.tinkerpop.blueprints.Query;
@@ -28,6 +30,8 @@ public class Imports {
         imports.add("org.apache.commons.configuration.*");
         imports.add("static " + Geo.class.getName() + ".*");
         imports.add("static " + Text.class.getName() + ".*");
+        imports.add("static " + Cardinality.class.getName() + ".*");
+        imports.add("static " + Multiplicity.class.getName() + ".*");
         // todo: remove with Gremlin 2.3.1+
         imports.add("static " + Query.Compare.class.getName() + ".*");
     }
