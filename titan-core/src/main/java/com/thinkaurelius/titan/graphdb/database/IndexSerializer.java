thinkaurelius/titan

commit 5a853d7a0866cc3eb0d65348303f7323386faa5d

Support for Contain.IN in queries-centric queries with test coverage. Addresses the second part of #679. Fixes #679.

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/database/IndexSerializer.java
@@ -10,6 +10,7 @@ import com.thinkaurelius.titan.core.schema.SchemaStatus;
 import com.thinkaurelius.titan.diskstorage.configuration.Configuration;
 import com.thinkaurelius.titan.graphdb.idmanagement.IDManager;
 import com.thinkaurelius.titan.graphdb.query.graph.GraphCentricQueryBuilder;
+import com.thinkaurelius.titan.graphdb.query.graph.MultiKeySliceQuery;
 import com.thinkaurelius.titan.graphdb.types.ParameterType;
 import com.thinkaurelius.titan.diskstorage.*;
 import com.thinkaurelius.titan.diskstorage.indexing.*;
@@ -505,19 +506,21 @@ public class IndexSerializer {
     public List<Object> query(final JointIndexQuery.Subquery query, final BackendTransaction tx) {
         IndexType index = query.getIndex();
         if (index.isCompositeIndex()) {
-            KeySliceQuery sq = query.getCompositeQuery();
-            EntryList r = tx.indexQuery(sq);
-            List<Object> results = new ArrayList<Object>(r.size());
-            for (java.util.Iterator<Entry> iterator = r.reuseIterator(); iterator.hasNext(); ) {
-                Entry entry = iterator.next();
-                ReadBuffer entryValue = entry.asReadBuffer();
-                entryValue.movePositionTo(entry.getValuePosition());
-                switch(index.getElement()) {
-                    case VERTEX:
-                        results.add(VariableLong.readPositive(entryValue));
-                        break;
-                    default:
-                        results.add(bytebuffer2RelationId(entryValue));
+            MultiKeySliceQuery sq = query.getCompositeQuery();
+            List<EntryList> rs = sq.execute(tx);
+            List<Object> results = new ArrayList<Object>(rs.get(0).size());
+            for (EntryList r : rs) {
+                for (java.util.Iterator<Entry> iterator = r.reuseIterator(); iterator.hasNext(); ) {
+                    Entry entry = iterator.next();
+                    ReadBuffer entryValue = entry.asReadBuffer();
+                    entryValue.movePositionTo(entry.getValuePosition());
+                    switch(index.getElement()) {
+                        case VERTEX:
+                            results.add(VariableLong.readPositive(entryValue));
+                            break;
+                        default:
+                            results.add(bytebuffer2RelationId(entryValue));
+                    }
                 }
             }
             boolean hasCardinalitySize = ((CompositeIndexType)index).getCardinality()!= Cardinality.SINGLE || results.size() <= 1;
@@ -531,8 +534,12 @@ public class IndexSerializer {
         }
     }
 
-    public KeySliceQuery getQuery(final CompositeIndexType index, Object[] values) {
-        return new KeySliceQuery(getIndexKey(index,values), BufferUtil.zeroBuffer(1), BufferUtil.oneBuffer(1));
+    public MultiKeySliceQuery getQuery(final CompositeIndexType index, List<Object[]> values) {
+        List<KeySliceQuery> ksqs = new ArrayList<KeySliceQuery>(values.size());
+        for (Object[] value : values) {
+            ksqs.add(new KeySliceQuery(getIndexKey(index,value), BufferUtil.zeroBuffer(1), BufferUtil.oneBuffer(1)));
+        }
+        return new MultiKeySliceQuery(ksqs);
     }
 
     public IndexQuery getQuery(final MixedIndexType index, final Condition condition, final OrderList orders) {

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/query/graph/MultiKeySliceQuery.java
@@ -1,0 +1,64 @@
+package com.thinkaurelius.titan.graphdb.query.graph;
+
+import com.google.common.base.Preconditions;
+import com.thinkaurelius.titan.diskstorage.BackendTransaction;
+import com.thinkaurelius.titan.diskstorage.EntryList;
+import com.thinkaurelius.titan.diskstorage.keycolumnvalue.KeySliceQuery;
+import com.thinkaurelius.titan.graphdb.query.BackendQuery;
+import com.thinkaurelius.titan.graphdb.query.BaseQuery;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Matthias Broecheler (me@matthiasb.com)
+ */
+public class MultiKeySliceQuery extends BaseQuery implements BackendQuery<MultiKeySliceQuery>  {
+
+    private final List<KeySliceQuery> queries;
+
+    public MultiKeySliceQuery(List<KeySliceQuery> queries) {
+        Preconditions.checkArgument(queries!=null && !queries.isEmpty());
+        this.queries = queries;
+    }
+
+    @Override
+    public MultiKeySliceQuery updateLimit(int newLimit) {
+        MultiKeySliceQuery newQuery = new MultiKeySliceQuery(queries);
+        newQuery.setLimit(newLimit);
+        return newQuery;
+    }
+
+    public List<EntryList> execute(final BackendTransaction tx) {
+        int total = 0;
+        List<EntryList> result = new ArrayList<EntryList>(4);
+        for (KeySliceQuery ksq : queries) {
+            EntryList next =tx.indexQuery(ksq.updateLimit(getLimit()-total));
+            result.add(next);
+            total+=next.size();
+            if (total>=getLimit()) break;
+        }
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(queries).appendSuper(super.hashCode()).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        else if (other == null) return false;
+        else if (!getClass().isInstance(other)) return false;
+        MultiKeySliceQuery oth = (MultiKeySliceQuery) other;
+        return getLimit()==oth.getLimit() && queries.equals(oth.queries);
+    }
+
+    @Override
+    public String toString() {
+        return "multiKSQ["+queries.size()+"]@"+getLimit();
+    }
+
+}

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/query/graph/GraphCentricQueryBuilder.java
@@ -17,6 +17,8 @@ import com.thinkaurelius.titan.graphdb.query.condition.*;
 import com.thinkaurelius.titan.graphdb.transaction.StandardTitanTx;
 import com.thinkaurelius.titan.graphdb.types.*;
 import com.thinkaurelius.titan.graphdb.types.system.ImplicitKey;
+import com.thinkaurelius.titan.util.datastructures.Interval;
+import com.thinkaurelius.titan.util.datastructures.PointInterval;
 import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
 import org.slf4j.Logger;
@@ -256,14 +258,16 @@ public class GraphCentricQueryBuilder implements TitanGraphQuery<GraphCentricQue
                 //Check that this index actually applies in case of a schema constraint
                 if (index.hasSchemaTypeConstraint()) {
                     TitanSchemaType type = index.getSchemaTypeConstraint();
-                    boolean matchesTypeConstraint = false;
-                    for (PredicateCondition<RelationType, TitanElement> atom : getEqualityPredicateConditions(conditions)) {
-                        if (atom.getKey().equals(ImplicitKey.LABEL) && type.getName().equals((String)atom.getValue())) {
-                            matchesTypeConstraint = true;
-                            subcover.add(atom);
-                        }
+                    Map.Entry<Condition,Collection<Object>> equalCon = getEqualityConditionValues(conditions,ImplicitKey.LABEL);
+                    if (equalCon==null) continue;
+                    Collection<Object> labels = equalCon.getValue();
+                    assert labels.size()>=1;
+                    if (labels.size()>1) {
+                        log.warn("The query optimizer currently does not support multiple label constraints in query: {}",this);
+                        continue;
                     }
-                    if (!matchesTypeConstraint) continue;
+                    if (!type.getName().equals((String)Iterables.getOnlyElement(labels))) continue;
+                    subcover.add(equalCon.getKey());
                 }
 
                 if (index.isCompositeIndex()) {
@@ -301,7 +305,7 @@ public class GraphCentricQueryBuilder implements TitanGraphQuery<GraphCentricQue
                 coveredClauses.addAll(candidateSubcover);
                 if (bestCandidate.isCompositeIndex()) {
                     jointQuery.add((CompositeIndexType)bestCandidate,
-                            serializer.getQuery((CompositeIndexType)bestCandidate,(Object[])candidateSubcondition));
+                            serializer.getQuery((CompositeIndexType)bestCandidate,(List<Object[]>)candidateSubcondition));
                 } else {
                     jointQuery.add((MixedIndexType)bestCandidate,
                             serializer.getQuery((MixedIndexType)bestCandidate,(Condition)candidateSubcondition,orders));
@@ -339,37 +343,59 @@ public class GraphCentricQueryBuilder implements TitanGraphQuery<GraphCentricQue
         return true;
     }
 
-    public static final Object[] indexCover(final CompositeIndexType index, Condition<TitanElement> condition, Set<Condition> covered) {
+    public static List<Object[]> indexCover(final CompositeIndexType index, Condition<TitanElement> condition, Set<Condition> covered) {
         assert QueryUtil.isQueryNormalForm(condition);
         assert condition instanceof And;
         if (index.getStatus()!= SchemaStatus.ENABLED) return null;
         IndexField[] fields = index.getFieldKeys();
-        Object[] indexCover = new Object[fields.length];
-        Condition[] coveredClauses = new Condition[fields.length];
-        for (int i = 0; i < fields.length; i++) {
-            IndexField field = fields[i];
-            for (PredicateCondition<RelationType, TitanElement> atom : getEqualityPredicateConditions(condition)) {
-                if (atom.getKey().equals(field.getFieldKey())) {
-                    indexCover[i]=atom.getValue();
-                    coveredClauses[i]=atom;
-                }
-            }
-            if (indexCover[i]==null) return null; //Couldn't find a match
-        }
-        assert indexCover!=null;
-        covered.addAll(Arrays.asList(coveredClauses));
-        return indexCover;
+        Object[] indexValues = new Object[fields.length];
+        Set<Condition> coveredClauses = new HashSet<Condition>(fields.length);
+        List<Object[]> indexCovers = new ArrayList<Object[]>(4);
+
+        constructIndexCover(indexValues,0,fields,condition,indexCovers,coveredClauses);
+        if (!indexCovers.isEmpty()) {
+            covered.addAll(coveredClauses);
+            return indexCovers;
+        } else return null;
     }
 
-    private static final Iterable<PredicateCondition<RelationType,TitanElement>> getEqualityPredicateConditions(Condition<TitanElement> condition) {
-        return (Iterable)Iterables.filter(condition.getChildren(),new Predicate<Condition<TitanElement>>() {
-            @Override
-            public boolean apply(@Nullable Condition<TitanElement> subclause) {
-                if (!(subclause instanceof PredicateCondition)) return false;
-                PredicateCondition<RelationType, TitanElement> atom = (PredicateCondition) subclause;
-                return atom.getPredicate()==Cmp.EQUAL && atom.getValue()!=null;
+    private static void constructIndexCover(Object[] indexValues, int position, IndexField[] fields,
+                                            Condition<TitanElement> condition,
+                                            List<Object[]> indexCovers, Set<Condition> coveredClauses) {
+        if (position>=fields.length) {
+            indexCovers.add(indexValues);
+        } else {
+            IndexField field = fields[position];
+            Map.Entry<Condition,Collection<Object>> equalCon = getEqualityConditionValues(condition,field.getFieldKey());
+            if (equalCon!=null) {
+                coveredClauses.add(equalCon.getKey());
+                assert equalCon.getValue().size()>0;
+                for (Object value : equalCon.getValue()) {
+                    Object[] newValues = Arrays.copyOf(indexValues,fields.length);
+                    newValues[position]=value;
+                    constructIndexCover(newValues,position+1,fields,condition,indexCovers,coveredClauses);
+                }
+            } else return;
+        }
+
+    }
+
+    private static final Map.Entry<Condition,Collection<Object>> getEqualityConditionValues(Condition<TitanElement> condition, RelationType type) {
+        for (Condition c : condition.getChildren()) {
+            if (c instanceof Or) {
+                Map.Entry<RelationType,Collection> orEqual = QueryUtil.extractOrCondition((Or)c);
+                if (orEqual!=null && orEqual.getKey().equals(type) && !orEqual.getValue().isEmpty()) {
+                    return new AbstractMap.SimpleImmutableEntry(c,orEqual.getValue());
+                }
+            } else if (c instanceof PredicateCondition) {
+                PredicateCondition<RelationType, TitanRelation> atom = (PredicateCondition)c;
+                if (atom.getKey().equals(type) && atom.getPredicate()==Cmp.EQUAL && atom.getValue()!=null) {
+                    return new AbstractMap.SimpleImmutableEntry(c,ImmutableList.of(atom.getValue()));
+                }
             }
-        });
+
+        }
+        return null;
     }
 
     public static final Condition<TitanElement> indexCover(final MixedIndexType index, Condition<TitanElement> condition,

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/query/graph/JointIndexQuery.java
@@ -3,7 +3,6 @@ package com.thinkaurelius.titan.graphdb.query.graph;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.thinkaurelius.titan.diskstorage.indexing.IndexQuery;
-import com.thinkaurelius.titan.diskstorage.keycolumnvalue.KeySliceQuery;
 import com.thinkaurelius.titan.graphdb.query.BackendQuery;
 import com.thinkaurelius.titan.graphdb.query.BaseQuery;
 import com.thinkaurelius.titan.graphdb.types.CompositeIndexType;
@@ -43,7 +42,7 @@ public class JointIndexQuery extends BaseQuery implements BackendQuery<JointInde
         queries.add(new Subquery(index, query));
     }
 
-    public void add(CompositeIndexType index, KeySliceQuery query) {
+    public void add(CompositeIndexType index, MultiKeySliceQuery query) {
         queries.add(new Subquery(index, query));
     }
 
@@ -91,7 +90,7 @@ public class JointIndexQuery extends BaseQuery implements BackendQuery<JointInde
         private final BackendQuery query;
 
         private Subquery(IndexType index, BackendQuery query) {
-            assert index!=null && query!=null && (query instanceof KeySliceQuery || query instanceof IndexQuery);
+            assert index!=null && query!=null && (query instanceof MultiKeySliceQuery || query instanceof IndexQuery);
             this.index = index;
             this.query = query;
         }
@@ -105,9 +104,9 @@ public class JointIndexQuery extends BaseQuery implements BackendQuery<JointInde
             return (IndexQuery)query;
         }
 
-        public KeySliceQuery getCompositeQuery() {
-            Preconditions.checkArgument(index.isCompositeIndex() && query instanceof KeySliceQuery);
-            return (KeySliceQuery)query;
+        public MultiKeySliceQuery getCompositeQuery() {
+            Preconditions.checkArgument(index.isCompositeIndex() && query instanceof MultiKeySliceQuery);
+            return (MultiKeySliceQuery)query;
         }
 
         @Override

titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanGraphTest.java
@@ -3749,6 +3749,8 @@ public abstract class TitanGraphTest extends TitanGraphBaseTest {
         //########## QUERIES ################
         evaluateQuery(tx.query().has(time,Cmp.EQUAL,10).has(weight,Cmp.EQUAL,0),
                 ElementCategory.EDGE,1,new boolean[]{true,sorted},edge1.getName());
+        evaluateQuery(tx.query().has(time,Contain.IN,ImmutableList.of(10,20,30)).has(weight,Cmp.EQUAL,0),
+                ElementCategory.EDGE,3,new boolean[]{true,sorted},edge1.getName());
         evaluateQuery(tx.query().has(time,Cmp.EQUAL,10).has(weight,Cmp.EQUAL,0).has(text,Cmp.EQUAL,strs[10%strs.length]),
                 ElementCategory.EDGE,1,new boolean[]{false,sorted},edge1.getName());
         evaluateQuery(tx.query().has(time,Cmp.EQUAL,10).has(weight,Cmp.EQUAL,1),
@@ -3787,6 +3789,8 @@ public abstract class TitanGraphTest extends TitanGraphBaseTest {
                 ElementCategory.VERTEX,1,new boolean[]{true,sorted},vertex2.getName());
         evaluateQuery(tx.query().has(time,Cmp.EQUAL,51).has(name,Cmp.EQUAL,"u1").has("label",org.getName()),
                 ElementCategory.VERTEX,1,new boolean[]{true,sorted},vertex2.getName());
+        evaluateQuery(tx.query().has(time,Contain.IN,ImmutableList.of(51,61,71,31,41)).has(name,Cmp.EQUAL,"u1").has("label",org.getName()),
+                ElementCategory.VERTEX,5,new boolean[]{true,sorted},vertex2.getName());
         evaluateQuery(tx.query().has(time,Cmp.EQUAL,51).has("label",org.getName()),
                 ElementCategory.VERTEX,1,new boolean[]{false,sorted});
         evaluateQuery(tx.query().has(name,Cmp.EQUAL,"u1"),
@@ -3810,6 +3814,8 @@ public abstract class TitanGraphTest extends TitanGraphBaseTest {
         //########## QUERIES (copied from above) ################
         evaluateQuery(tx.query().has(time,Cmp.EQUAL,10).has(weight,Cmp.EQUAL,0),
                 ElementCategory.EDGE,1,new boolean[]{true,sorted},edge1.getName());
+        evaluateQuery(tx.query().has(time,Contain.IN,ImmutableList.of(10,20,30)).has(weight,Cmp.EQUAL,0),
+                ElementCategory.EDGE,3,new boolean[]{true,sorted},edge1.getName());
         evaluateQuery(tx.query().has(time,Cmp.EQUAL,10).has(weight,Cmp.EQUAL,0).has(text,Cmp.EQUAL,strs[10%strs.length]),
                 ElementCategory.EDGE,1,new boolean[]{false,sorted},edge1.getName());
         evaluateQuery(tx.query().has(time,Cmp.EQUAL,10).has(weight,Cmp.EQUAL,1),
@@ -3848,6 +3854,8 @@ public abstract class TitanGraphTest extends TitanGraphBaseTest {
                 ElementCategory.VERTEX,1,new boolean[]{true,sorted},vertex2.getName());
         evaluateQuery(tx.query().has(time,Cmp.EQUAL,51).has(name,Cmp.EQUAL,"u1").has("label",org.getName()),
                 ElementCategory.VERTEX,1,new boolean[]{true,sorted},vertex2.getName());
+        evaluateQuery(tx.query().has(time,Contain.IN,ImmutableList.of(51,61,71,31,41)).has(name,Cmp.EQUAL,"u1").has("label",org.getName()),
+                ElementCategory.VERTEX,5,new boolean[]{true,sorted},vertex2.getName());
         evaluateQuery(tx.query().has(time,Cmp.EQUAL,51).has("label",org.getName()),
                 ElementCategory.VERTEX,1,new boolean[]{false,sorted});
         evaluateQuery(tx.query().has(name,Cmp.EQUAL,"u1"),

commit 401d88ecdd4a16eb5cb0d88e29af80856d46e907

Push down MultiKeySliceQuery cardinality check
The IndexSerializer.query precondition failing in #882 was introduced
in 5a853d7a0866cc3eb0d65348303f7323386faa5d.  This commit also
introduced MultiKeySliceQuery (which has not been modified since)
which is involved in the precondition.

The precondition fails when executing queries like this:

graph.query()
     .has('label', ...)
     .has(uniqueCompositeIndexedKey, Contain.IN, ...)
     .vertices()

where multiple vertices have the specified label and satisfy the
Contain.IN predicate.  The composite index serving the query has
Cardinality.SINGLE, but the MultiKSQ returns more than one vertex,
failing the precondition.

I suspect, though I'm not certain, that this may reflect a shortcoming
in the precondition.  This commit moves the precondition down from
IndexSerializer.query to MultiKeySliceQuery.execute (the latter is
called from and only from the former).  The new precondition assumes
that each individual subquery inside the MultiKSQ will return at most
one result when the index in question has Cardinality.SINGLE.  The
difference is basically: before, we checked whether the aggregate
result count of a MultiKSQ <= 1, now we check whether the result count
of each individual subquery in the MultiKSQ <= 1 (when the index in
question is SINGLE).  Not sure that's right though.

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/database/IndexSerializer.java
@@ -507,7 +507,7 @@ public class IndexSerializer {
         IndexType index = query.getIndex();
         if (index.isCompositeIndex()) {
             MultiKeySliceQuery sq = query.getCompositeQuery();
-            List<EntryList> rs = sq.execute(tx);
+            List<EntryList> rs = sq.execute(tx, ((CompositeIndexType)index).getCardinality());
             List<Object> results = new ArrayList<Object>(rs.get(0).size());
             for (EntryList r : rs) {
                 for (java.util.Iterator<Entry> iterator = r.reuseIterator(); iterator.hasNext(); ) {
@@ -523,8 +523,6 @@ public class IndexSerializer {
                     }
                 }
             }
-            boolean hasCardinalitySize = ((CompositeIndexType)index).getCardinality()!= Cardinality.SINGLE || results.size() <= 1;
-            Preconditions.checkArgument(((CompositeIndexType)index).getCardinality()!=Cardinality.SINGLE || results.size() <= 1);
             return results;
         } else {
             List<String> r = tx.indexQuery(((MixedIndexType) index).getBackingIndexName(), query.getMixedQuery());

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/query/graph/MultiKeySliceQuery.java
@@ -1,6 +1,7 @@
 package com.thinkaurelius.titan.graphdb.query.graph;
 
 import com.google.common.base.Preconditions;
+import com.thinkaurelius.titan.core.Cardinality;
 import com.thinkaurelius.titan.diskstorage.BackendTransaction;
 import com.thinkaurelius.titan.diskstorage.EntryList;
 import com.thinkaurelius.titan.diskstorage.keycolumnvalue.KeySliceQuery;
@@ -30,13 +31,15 @@ public class MultiKeySliceQuery extends BaseQuery implements BackendQuery<MultiK
         return newQuery;
     }
 
-    public List<EntryList> execute(final BackendTransaction tx) {
+    public List<EntryList> execute(final BackendTransaction tx, Cardinality indexCardinality) {
         int total = 0;
         List<EntryList> result = new ArrayList<EntryList>(4);
         for (KeySliceQuery ksq : queries) {
             EntryList next =tx.indexQuery(ksq.updateLimit(getLimit()-total));
+            int nextSize = next.size();
             result.add(next);
-            total+=next.size();
+            Preconditions.checkArgument(indexCardinality != Cardinality.SINGLE || nextSize <= 1);
+            total+=nextSize;
             if (total>=getLimit()) break;
         }
         return result;