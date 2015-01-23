278d8b988058183ed032b03959771864f6b37a0c
Matthias Broecheler <me@matthiasb.com>
2015-01-22 17:54:37 -0800

    Added metrics instrumentation around manager to account for mutateMany calls. Refactored metrics initialization.

--- titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/Backend.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/Backend.java
@@ -29,6 +29,7 @@ import com.thinkaurelius.titan.diskstorage.log.kcvs.KCVSLogManager;
 import com.thinkaurelius.titan.diskstorage.util.BackendOperation;
 import com.thinkaurelius.titan.diskstorage.util.MetricInstrumentedStore;
 import com.thinkaurelius.titan.diskstorage.configuration.backend.KCVSConfiguration;
+import com.thinkaurelius.titan.diskstorage.util.MetricInstrumentedStoreManager;
 import com.thinkaurelius.titan.diskstorage.util.StandardBaseTransactionConfig;
 import com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration;
 import com.thinkaurelius.titan.graphdb.configuration.TitanConstants;
@@ -74,6 +75,7 @@ public class Backend implements LockerProvider {
 
     public static final String ID_STORE_NAME = "titan_ids";
 
+    public static final String METRICS_STOREMANAGER_NAME = "storeManager";
     public static final String METRICS_MERGED_STORE = "stores";
     public static final String METRICS_MERGED_CACHE = "caches";
     public static final String METRICS_CACHE_SUFFIX = ".cache";
@@ -128,7 +130,12 @@ public class Backend implements LockerProvider {
     public Backend(Configuration configuration) {
         this.configuration = configuration;
 
-        storeManager = getStorageManager(configuration);
+        KeyColumnValueStoreManager manager = getStorageManager(configuration);
+        if (configuration.get(BASIC_METRICS)) {
+            storeManager = new MetricInstrumentedStoreManager(manager,METRICS_STOREMANAGER_NAME,configuration.get(METRICS_MERGE_STORES),METRICS_MERGED_STORE);
+        } else {
+            storeManager = manager;
+        }
         indexes = getIndexes(configuration);
         storeFeatures = storeManager.getFeatures();
 
@@ -206,13 +213,9 @@ public class Backend implements LockerProvider {
      */
     public void initialize(Configuration config) {
         try {
-            boolean reportMetrics = configuration.get(BASIC_METRICS);
-
             //EdgeStore & VertexIndexStore
             KeyColumnValueStore idStore = storeManager.openDatabase(ID_STORE_NAME);
-            if (reportMetrics) {
-                idStore = new MetricInstrumentedStore(idStore, getMetricsStoreName(ID_STORE_NAME));
-            }
+
             idAuthority = null;
             if (storeFeatures.isKeyConsistent()) {
                 idAuthority = new ConsistentKeyIDAuthority(idStore, storeManager, config);
@@ -222,11 +225,6 @@ public class Backend implements LockerProvider {
 
             KeyColumnValueStore edgeStoreRaw = storeManagerLocking.openDatabase(EDGESTORE_NAME);
             KeyColumnValueStore indexStoreRaw = storeManagerLocking.openDatabase(INDEXSTORE_NAME);
-
-            if (reportMetrics) {
-                edgeStoreRaw = new MetricInstrumentedStore(edgeStoreRaw, getMetricsStoreName(EDGESTORE_NAME));
-                indexStoreRaw = new MetricInstrumentedStore(indexStoreRaw, getMetricsStoreName(INDEXSTORE_NAME));
-            }
 
             //Configure caches
             if (cacheEnabled) {
@@ -251,8 +249,8 @@ public class Backend implements LockerProvider {
                 long edgeStoreCacheSize = Math.round(cacheSizeBytes * EDGESTORE_CACHE_PERCENT);
                 long indexStoreCacheSize = Math.round(cacheSizeBytes * INDEXSTORE_CACHE_PERCENT);
 
-                edgeStore = new ExpirationKCVSCache(edgeStoreRaw,getMetricsCacheName("edgeStore",reportMetrics),expirationTime,cleanWaitTime,edgeStoreCacheSize);
-                indexStore = new ExpirationKCVSCache(indexStoreRaw,getMetricsCacheName("indexStore",reportMetrics),expirationTime,cleanWaitTime,indexStoreCacheSize);
+                edgeStore = new ExpirationKCVSCache(edgeStoreRaw,getMetricsCacheName("edgeStore"),expirationTime,cleanWaitTime,edgeStoreCacheSize);
+                indexStore = new ExpirationKCVSCache(indexStoreRaw,getMetricsCacheName("indexStore"),expirationTime,cleanWaitTime,indexStoreCacheSize);
             } else {
                 edgeStore = new NoKCVSCache(edgeStoreRaw);
                 indexStore = new NoKCVSCache(indexStoreRaw);
@@ -331,12 +329,8 @@ public class Backend implements LockerProvider {
         return systemConfig;
     }
 
-    private String getMetricsStoreName(String storeName) {
-        return configuration.get(METRICS_MERGE_STORES) ? METRICS_MERGED_STORE : storeName;
-    }
-
-    private String getMetricsCacheName(String storeName, boolean reportMetrics) {
-        if (!reportMetrics) return null;
+    private String getMetricsCacheName(String storeName) {
+        if (!configuration.get(BASIC_METRICS)) return null;
         return configuration.get(METRICS_MERGE_STORES) ? METRICS_MERGED_CACHE : storeName + METRICS_CACHE_SUFFIX;
     }
 
