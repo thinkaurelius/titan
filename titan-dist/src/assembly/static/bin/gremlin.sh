5298f5d923fd2fbf276b40d7c20d4d56fd2f0621
Dan LaRocque <dalaro@hopcount.org>
2015-02-10 00:38:41 -0500

    Optionify the # of result elems shown in the shell
    Since 645b406468f06c043b810375d6203618b505b241 made the Console.LINES
    limit apply non-hadoop result collections for what appears to be the
    first time, it's possible that the 15 element limit may surprise some
    people and prove annoyingly low.
    
    This commit retains the 15 element limit default, but it makes the
    actual number of elements configurable on the gremlin.sh command line
    via the -w <number> option, e.g.
    
      bin/gremlin.sh -w 50
    
    will print up to fifty elements from a result collection before
    truncating any remaining elements (and if at least one element remains
    after those initial fifty, printing a set of ellipses).
    
    It may be annoying, but at least it's no longer hardcoded.
    
    I don't think this breaks compatibility with existing scripts, unless
    there's someone out there naming their Groovy script files "-w".  We
    don't have a "--" option to disambiguate.

--- titan-dist/src/assembly/static/bin/gremlin.sh
+++ titan-dist/src/assembly/static/bin/gremlin.sh
@@ -114,10 +114,18 @@ if [ -z "${SCRIPT_DEBUG:-}" ]; then
     SCRIPT_DEBUG=
 fi
 
+# The maximum number of elements of a collection enumerated by the
+# shell before printing ellipses ("...") and omitting the remaining
+# elements.  Zero or less prints collections in full and never omits
+# collection elements.
+if [ -z "${TITAN_RESULT_WINDOW_SIZE:-}" ]; then
+    TITAN_RESULT_WINDOW_SIZE=15
+fi
+
 # Process options
 MAIN_CLASS=com.thinkaurelius.titan.hadoop.tinkerpop.gremlin.Console
 
-while getopts "eilv" opt; do
+while getopts "eilvw" opt; do
     case "$opt" in
     e) MAIN_CLASS=com.thinkaurelius.titan.hadoop.tinkerpop.gremlin.ScriptExecutor
        # For compatibility with behavior pre-Titan-0.5.0, stop
@@ -138,7 +146,9 @@ while getopts "eilv" opt; do
 	   SCRIPT_DEBUG=y
        fi
        ;;
-    v) MAIN_CLASS=com.tinkerpop.gremlin.Version
+    v) MAIN_CLASS=com.tinkerpop.gremlin.Version ;;
+    w) eval TITAN_RESULT_WINDOW_SIZE=\$$OPTIND
+       OPTIND="$(( $OPTIND + 1 ))" ;;
     esac
 done
 
@@ -154,6 +164,8 @@ fi
 if [ -n "${TITAN_JAVA_OPTS:-}" ]; then
     JAVA_OPTIONS="$JAVA_OPTIONS $TITAN_JAVA_OPTS"
 fi
+
+export TITAN_RESULT_WINDOW_SIZE
 
 if [ -n "$SCRIPT_DEBUG" ]; then
     echo "CLASSPATH: $CLASSPATH"
--- titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/tinkerpop/gremlin/ResultHookClosure.java
+++ titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/tinkerpop/gremlin/ResultHookClosure.java
@@ -13,6 +13,8 @@ import groovy.lang.Closure;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.codehaus.groovy.tools.shell.IO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Iterator;
 
@@ -20,9 +22,59 @@ import java.util.Iterator;
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
 public class ResultHookClosure extends Closure {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(ResultHookClosure.class);
+
+    /**
+     * This environment variable is likely to be changed or removed in future
+     * versions of Titan, particularly Titan releases based on TinkerPop 3 and later.
+     *
+     * This would ideally be handled by the Console main class using JCommander
+     * or commons-cli or args4j etc to parse its main-method arguments instead of
+     * relying on an environment variable, but I don't want to rewrite Console's
+     * argument handling for one option.  Looking for a tipping point.
+     *
+     * Do not assume clean upgrades of scripts that rely on setting this
+     * environment variable directly to control the shell' collection enumeration limit.
+     */
+    private static final String LINES_ENV_VAR = "TITAN_RESULT_WINDOW_SIZE";
+    private static final int LINES_MIN = 1;
+    private static final int LINES;
+
     private final String resultPrompt;
     private final IO io;
-    private static final int LINES = 15;
+
+    static {
+        int l = 15;
+
+        /* If this code contains an overlooked exception throw site, and that
+         * exception propagates out of the static initializer, then this class
+         * will fail to init and the Console will refuse to start, even though
+         * we're just trying to read a silly cosmetic variable.  Catching
+         * Throwable out of paranoia.
+         */
+        try {
+            String raw = System.getenv(LINES_ENV_VAR);
+            if (null != raw) {
+                try {
+                    l = Integer.parseInt(raw);
+                    log.debug("Read environment variable {}={}", LINES_ENV_VAR, l);
+                    if (l < LINES_MIN) {
+                        log.debug("Configured value {} for {} is too low; increasing to minimum value {}",
+                                l, LINES_ENV_VAR, LINES_MIN);
+                        l = LINES_MIN;
+                    }
+                } catch (NumberFormatException e) {
+                    log.warn("Unable to parse environment variable {}={}", LINES_ENV_VAR, raw, e);
+                }
+            }
+        } catch (Throwable t) {
+            log.warn("Could not read environment variable {}", LINES_ENV_VAR);
+        }
+
+        LINES = l;
+    }
 
     public ResultHookClosure(final Object owner, final IO io, final String resultPrompt) {
         super(owner);
