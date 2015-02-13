thinkaurelius/titan

commit 278d8b988058183ed032b03959771864f6b37a0c

Added metrics instrumentation around manager to account for mutateMany calls. Refactored metrics initialization.

titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanOperationCountingTest.java
@@ -202,9 +202,32 @@ public abstract class TitanOperationCountingTest extends TitanGraphBaseTest {
 
     }
 
-
     public static final List<String> STORE_NAMES =
-            ImmutableList.of("edgeStore", "vertexIndexStore", "edgeIndexStore", "idStore");
+            ImmutableList.of(Backend.EDGESTORE_NAME,Backend.INDEXSTORE_NAME,Backend.ID_STORE_NAME,Backend.METRICS_STOREMANAGER_NAME);
+
+    @Test
+    public void testSettingProperty() throws Exception {
+        metricsPrefix = "metrics1";
+
+        mgmt.makePropertyKey("foo").dataType(String.class).cardinality(Cardinality.SINGLE).make();
+        finishSchema();
+
+        TitanVertex v = tx.addVertex();
+        v.setProperty("foo","bar");
+        tx.commit();
+
+
+        TitanTransaction tx = graph.buildTransaction().checkExternalVertexExistence(false).setGroupName(metricsPrefix).start();
+        v = tx.getVertex(v.getLongId());
+        v.setProperty("foo","bus");
+        tx.commit();
+        printAllMetrics();
+        verifyStoreMetrics(STORE_NAMES.get(0));
+        verifyStoreMetrics(STORE_NAMES.get(1));
+        verifyStoreMetrics(STORE_NAMES.get(2));
+        verifyStoreMetrics(STORE_NAMES.get(3), ImmutableMap.of(M_MUTATE, 1l));
+    }
+
 
     @Test
     @Ignore //TODO: Ignore for now until everything is stable - then do the counting

titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/Backend.java
@@ -29,6 +29,7 @@ import com.thinkaurelius.titan.diskstorage.log.kcvs.KCVSLogManager;
 import com.thinkaurelius.titan.diskstorage.util.BackendOperation;
 import com.thinkaurelius.titan.diskstorage.util.MetricInstrumentedStore;
 import com.thinkaurelius.titan.diskstorage.configuration.backend.KCVSConfiguration;
+import com.thinkaurelius.titan.diskstorage.util.MetricInstrumentedStoreManager;
 import com.thinkaurelius.titan.diskstorage.util.StandardBaseTransactionConfig;
 import com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration;
 import com.thinkaurelius.titan.graphdb.configuration.TitanConstants;
@@ -74,6 +75,7 @@ public class Backend implements LockerProvider {
 
     public static final String ID_STORE_NAME = "titan_ids";
 
+    public static final String METRICS_STOREMANAGER_NAME = "storeManager";
     public static final String METRICS_MERGED_STORE = "stores";
     public static final String METRICS_MERGED_CACHE = "caches";
     public static final String METRICS_CACHE_SUFFIX = ".cache";
@@ -128,7 +130,12 @@ public class Backend implements LockerProvider {
     public Backend(Configuration configuration) {
         this.configuration = configuration;
 
-        storeManager = getStorageManager(configuration);
+        KeyColumnValueStoreManager manager = getStorageManager(configuration);
+        if (configuration.get(BASIC_METRICS)) {
+            storeManager = new MetricInstrumentedStoreManager(manager,METRICS_STOREMANAGER_NAME,configuration.get(METRICS_MERGE_STORES),METRICS_MERGED_STORE);
+        } else {
+            storeManager = manager;
+        }
         indexes = getIndexes(configuration);
         storeFeatures = storeManager.getFeatures();
 
@@ -206,13 +213,9 @@ public class Backend implements LockerProvider {
      */
     public void initialize(Configuration config) {
         try {
-            boolean reportMetrics = configuration.get(BASIC_METRICS);
-
             //EdgeStore & VertexIndexStore
             KeyColumnValueStore idStore = storeManager.openDatabase(ID_STORE_NAME);
-            if (reportMetrics) {
-                idStore = new MetricInstrumentedStore(idStore, getMetricsStoreName(ID_STORE_NAME));
-            }
+
             idAuthority = null;
             if (storeFeatures.isKeyConsistent()) {
                 idAuthority = new ConsistentKeyIDAuthority(idStore, storeManager, config);
@@ -222,11 +225,6 @@ public class Backend implements LockerProvider {
 
             KeyColumnValueStore edgeStoreRaw = storeManagerLocking.openDatabase(EDGESTORE_NAME);
             KeyColumnValueStore indexStoreRaw = storeManagerLocking.openDatabase(INDEXSTORE_NAME);
-
-            if (reportMetrics) {
-                edgeStoreRaw = new MetricInstrumentedStore(edgeStoreRaw, getMetricsStoreName(EDGESTORE_NAME));
-                indexStoreRaw = new MetricInstrumentedStore(indexStoreRaw, getMetricsStoreName(INDEXSTORE_NAME));
-            }
 
             //Configure caches
             if (cacheEnabled) {
@@ -251,8 +249,8 @@ public class Backend implements LockerProvider {
                 long edgeStoreCacheSize = Math.round(cacheSizeBytes * EDGESTORE_CACHE_PERCENT);
                 long indexStoreCacheSize = Math.round(cacheSizeBytes * INDEXSTORE_CACHE_PERCENT);
 
-                edgeStore = new ExpirationKCVSCache(edgeStoreRaw,getMetricsCacheName("edgeStore",reportMetrics),expirationTime,cleanWaitTime,edgeStoreCacheSize);
-                indexStore = new ExpirationKCVSCache(indexStoreRaw,getMetricsCacheName("indexStore",reportMetrics),expirationTime,cleanWaitTime,indexStoreCacheSize);
+                edgeStore = new ExpirationKCVSCache(edgeStoreRaw,getMetricsCacheName("edgeStore"),expirationTime,cleanWaitTime,edgeStoreCacheSize);
+                indexStore = new ExpirationKCVSCache(indexStoreRaw,getMetricsCacheName("indexStore"),expirationTime,cleanWaitTime,indexStoreCacheSize);
             } else {
                 edgeStore = new NoKCVSCache(edgeStoreRaw);
                 indexStore = new NoKCVSCache(indexStoreRaw);
@@ -331,12 +329,8 @@ public class Backend implements LockerProvider {
         return systemConfig;
     }
 
-    private String getMetricsStoreName(String storeName) {
-        return configuration.get(METRICS_MERGE_STORES) ? METRICS_MERGED_STORE : storeName;
-    }
-
-    private String getMetricsCacheName(String storeName, boolean reportMetrics) {
-        if (!reportMetrics) return null;
+    private String getMetricsCacheName(String storeName) {
+        if (!configuration.get(BASIC_METRICS)) return null;
         return configuration.get(METRICS_MERGE_STORES) ? METRICS_MERGED_CACHE : storeName + METRICS_CACHE_SUFFIX;
     }
 

titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/util/MetricInstrumentedStoreManager.java
@@ -1,0 +1,108 @@
+package com.thinkaurelius.titan.diskstorage.util;
+
+import com.codahale.metrics.Timer;
+import com.google.common.base.Preconditions;
+import com.thinkaurelius.titan.diskstorage.BackendException;
+import com.thinkaurelius.titan.diskstorage.BaseTransactionConfig;
+import com.thinkaurelius.titan.diskstorage.StaticBuffer;
+import com.thinkaurelius.titan.diskstorage.keycolumnvalue.*;
+import com.thinkaurelius.titan.util.stats.MetricManager;
+import static com.thinkaurelius.titan.diskstorage.util.MetricInstrumentedStore.*;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration.METRICS_MERGE_STORES;
+
+/**
+ * @author Matthias Broecheler (me@matthiasb.com)
+ */
+public class MetricInstrumentedStoreManager implements KeyColumnValueStoreManager {
+
+    public static final String M_OPEN_DATABASE = "openDatabase";
+    public static final String M_START_TX = "startTransaction";
+    public static final String M_CLOSE_MANAGER = "closeManager";
+
+
+    public static final String GLOBAL_PREFIX = "global";
+
+    private final KeyColumnValueStoreManager backend;
+    private final boolean mergeStoreMetrics;
+    private final String mergedMetricsName;
+    private final String managerMetricsName;
+
+    public MetricInstrumentedStoreManager(KeyColumnValueStoreManager backend, String managerMetricsName,
+                                          boolean mergeStoreMetrics, String mergedMetricsName) {
+        this.backend = backend;
+        this.mergeStoreMetrics = mergeStoreMetrics;
+        this.mergedMetricsName = mergedMetricsName;
+        this.managerMetricsName = managerMetricsName;
+    }
+
+
+    private String getMetricsStoreName(String storeName) {
+        return mergeStoreMetrics ? mergedMetricsName : storeName;
+    }
+
+    @Override
+    public KeyColumnValueStore openDatabase(String name) throws BackendException {
+        MetricManager.INSTANCE.getCounter(GLOBAL_PREFIX, managerMetricsName, M_OPEN_DATABASE, M_CALLS).inc();
+        return new MetricInstrumentedStore(backend.openDatabase(name),getMetricsStoreName(name));
+    }
+
+    @Override
+    public void mutateMany(Map<String, Map<StaticBuffer, KCVMutation>> mutations, StoreTransaction txh) throws BackendException {
+        if (!txh.getConfiguration().hasGroupName()) {
+            backend.mutateMany(mutations,txh);
+        }
+        String prefix = txh.getConfiguration().getGroupName();
+
+        final MetricManager mgr = MetricManager.INSTANCE;
+        mgr.getCounter(prefix, managerMetricsName, M_MUTATE, M_CALLS).inc();
+        final Timer.Context tc = mgr.getTimer(prefix,  managerMetricsName, M_MUTATE, M_TIME).time();
+
+        try {
+            backend.mutateMany(mutations,txh);
+        } catch (BackendException e) {
+            mgr.getCounter(prefix,  managerMetricsName, M_MUTATE, M_EXCEPTIONS).inc();
+            throw e;
+        } catch (RuntimeException e) {
+            mgr.getCounter(prefix,  managerMetricsName, M_MUTATE, M_EXCEPTIONS).inc();
+            throw e;
+        } finally {
+            tc.stop();
+        }
+    }
+
+    @Override
+    public StoreTransaction beginTransaction(BaseTransactionConfig config) throws BackendException {
+        MetricManager.INSTANCE.getCounter(GLOBAL_PREFIX, managerMetricsName, M_START_TX, M_CALLS).inc();
+        return backend.beginTransaction(config);
+    }
+
+    @Override
+    public void close() throws BackendException {
+        backend.close();
+        MetricManager.INSTANCE.getCounter(GLOBAL_PREFIX, managerMetricsName, M_CLOSE_MANAGER, M_CALLS).inc();
+    }
+
+    @Override
+    public void clearStorage() throws BackendException {
+        backend.clearStorage();
+    }
+
+    @Override
+    public StoreFeatures getFeatures() {
+        return backend.getFeatures();
+    }
+
+    @Override
+    public String getName() {
+        return backend.getName();
+    }
+
+    @Override
+    public List<KeyRange> getLocalKeyPartition() throws BackendException {
+        return backend.getLocalKeyPartition();
+    }
+}

commit ee8f75a604071db919f0a18a11a1bc72cc1c883f

Update TitanOperationCountingTest magic numbers
This commit fixes expected method invocation counts for getSlice and
acquireLock in TitanOperationCountingTest assertions broken by
278d8b988058183ed032b03959771864f6b37a0c.

That commit moved the code that increments counters for KCVS.* or
StoreManager.mutateMany method calls such that BDB still passes, but
stores that use the consistent key locker (Cassandra + HBase) fail.
The counting now seems to happen after
ExpectedValueCheckingStore/Manager method is invoked, but before the
HBaseKCVS/Manager or Cassandra*KCVS/Manager method is invoked.  Hence
acquireLock is apparently never invoked, and getSlice is subject to
additional invocations as the ExpectedValue classes verify that the
actual stored values match those required by lock requests.

titan-berkeleyje/src/test/java/com/thinkaurelius/titan/graphdb/berkeleyje/BerkeleyOperationCountingTest.java
@@ -12,4 +12,9 @@ public class BerkeleyOperationCountingTest extends TitanOperationCountingTest {
         return BerkeleyStorageSetup.getBerkeleyJEGraphConfiguration();
     }
 
+    @Override
+    public boolean storeUsesConsistentKeyLocker() {
+        return false;
+    }
+
 }

titan-cassandra/src/test/java/com/thinkaurelius/titan/graphdb/thrift/ThriftOperationCountingTest.java
@@ -17,4 +17,8 @@ public class ThriftOperationCountingTest extends TitanOperationCountingTest {
         return CassandraStorageSetup.getCassandraThriftGraphConfiguration(getClass().getSimpleName());
     }
 
+    @Override
+    public boolean storeUsesConsistentKeyLocker() {
+        return true;
+    }
 }

titan-hbase-parent/titan-hbase-core/src/test/java/com/thinkaurelius/titan/graphdb/hbase/HBaseOperationCountingTest.java
@@ -20,6 +20,11 @@ public class HBaseOperationCountingTest extends TitanOperationCountingTest {
         return HBaseStorageSetup.getHBaseGraphConfiguration();
     }
 
+    @Override
+    public boolean storeUsesConsistentKeyLocker() {
+        return true;
+    }
+
     @AfterClass
     public static void stopHBase() {
         // Workaround for https://issues.apache.org/jira/browse/HBASE-10312

titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanOperationCountingTest.java
@@ -57,6 +57,8 @@ public abstract class TitanOperationCountingTest extends TitanGraphBaseTest {
 
     public abstract WriteConfiguration getBaseConfiguration();
 
+    public abstract boolean storeUsesConsistentKeyLocker();
+
     @Override
     public WriteConfiguration getConfiguration() {
         WriteConfiguration config = getBaseConfiguration();
@@ -113,7 +115,10 @@ public abstract class TitanOperationCountingTest extends TitanGraphBaseTest {
         tx.makeVertexLabel("person").make();
         tx.commit();
         verifyStoreMetrics(EDGESTORE_NAME);
-        verifyStoreMetrics(INDEXSTORE_NAME, ImmutableMap.of(M_GET_SLICE, 3l, M_ACQUIRE_LOCK, 3l));
+        if (storeUsesConsistentKeyLocker())
+            verifyStoreMetrics(INDEXSTORE_NAME, ImmutableMap.of(M_GET_SLICE, 6l, M_ACQUIRE_LOCK, 0l));
+        else
+            verifyStoreMetrics(INDEXSTORE_NAME, ImmutableMap.of(M_GET_SLICE, 3l, M_ACQUIRE_LOCK, 3l));
 
         resetMetrics();
 
@@ -177,7 +182,10 @@ public abstract class TitanOperationCountingTest extends TitanGraphBaseTest {
         e.setProperty("name","edge");
         tx.commit();
         verifyStoreMetrics(EDGESTORE_NAME);
-        verifyStoreMetrics(INDEXSTORE_NAME, ImmutableMap.of(M_GET_SLICE, 1l, M_ACQUIRE_LOCK, 1l));
+        if (storeUsesConsistentKeyLocker())
+            verifyStoreMetrics(INDEXSTORE_NAME, ImmutableMap.of(M_GET_SLICE, 2L, M_ACQUIRE_LOCK, 0L));
+        else
+            verifyStoreMetrics(INDEXSTORE_NAME, ImmutableMap.of(M_GET_SLICE, 1l, M_ACQUIRE_LOCK, 1l));
 
         for (int i = 1; i <= 30; i++) {
             metricsPrefix = "op"+i+cache;
@@ -221,7 +229,10 @@ public abstract class TitanOperationCountingTest extends TitanGraphBaseTest {
         v = tx.getVertex(v.getLongId());
         v.setProperty("foo", "bus");
         tx.commit();
-        verifyStoreMetrics(STORE_NAMES.get(0));
+        if (storeUsesConsistentKeyLocker())
+            verifyStoreMetrics(STORE_NAMES.get(1));
+        else
+            verifyStoreMetrics(STORE_NAMES.get(0));
         verifyStoreMetrics(STORE_NAMES.get(1));
         verifyStoreMetrics(STORE_NAMES.get(2));
         verifyStoreMetrics(STORE_NAMES.get(3), ImmutableMap.of(M_MUTATE, 1l));