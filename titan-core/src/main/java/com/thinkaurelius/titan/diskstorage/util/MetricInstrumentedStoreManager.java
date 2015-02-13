thinkaurelius/titan

commit 278d8b988058183ed032b03959771864f6b37a0c

Added metrics instrumentation around manager to account for mutateMany calls. Refactored metrics initialization.

titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/util/MetricInstrumentedStoreManager.java
@@ -1,0 +1,108 @@
+package com.thinkaurelius.titan.diskstorage.util;
+
+import com.codahale.metrics.Timer;
+import com.google.common.base.Preconditions;
+import com.thinkaurelius.titan.diskstorage.BackendException;
+import com.thinkaurelius.titan.diskstorage.BaseTransactionConfig;
+import com.thinkaurelius.titan.diskstorage.StaticBuffer;
+import com.thinkaurelius.titan.diskstorage.keycolumnvalue.*;
+import com.thinkaurelius.titan.util.stats.MetricManager;
+import static com.thinkaurelius.titan.diskstorage.util.MetricInstrumentedStore.*;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration.METRICS_MERGE_STORES;
+
+/**
+ * @author Matthias Broecheler (me@matthiasb.com)
+ */
+public class MetricInstrumentedStoreManager implements KeyColumnValueStoreManager {
+
+    public static final String M_OPEN_DATABASE = "openDatabase";
+    public static final String M_START_TX = "startTransaction";
+    public static final String M_CLOSE_MANAGER = "closeManager";
+
+
+    public static final String GLOBAL_PREFIX = "global";
+
+    private final KeyColumnValueStoreManager backend;
+    private final boolean mergeStoreMetrics;
+    private final String mergedMetricsName;
+    private final String managerMetricsName;
+
+    public MetricInstrumentedStoreManager(KeyColumnValueStoreManager backend, String managerMetricsName,
+                                          boolean mergeStoreMetrics, String mergedMetricsName) {
+        this.backend = backend;
+        this.mergeStoreMetrics = mergeStoreMetrics;
+        this.mergedMetricsName = mergedMetricsName;
+        this.managerMetricsName = managerMetricsName;
+    }
+
+
+    private String getMetricsStoreName(String storeName) {
+        return mergeStoreMetrics ? mergedMetricsName : storeName;
+    }
+
+    @Override
+    public KeyColumnValueStore openDatabase(String name) throws BackendException {
+        MetricManager.INSTANCE.getCounter(GLOBAL_PREFIX, managerMetricsName, M_OPEN_DATABASE, M_CALLS).inc();
+        return new MetricInstrumentedStore(backend.openDatabase(name),getMetricsStoreName(name));
+    }
+
+    @Override
+    public void mutateMany(Map<String, Map<StaticBuffer, KCVMutation>> mutations, StoreTransaction txh) throws BackendException {
+        if (!txh.getConfiguration().hasGroupName()) {
+            backend.mutateMany(mutations,txh);
+        }
+        String prefix = txh.getConfiguration().getGroupName();
+
+        final MetricManager mgr = MetricManager.INSTANCE;
+        mgr.getCounter(prefix, managerMetricsName, M_MUTATE, M_CALLS).inc();
+        final Timer.Context tc = mgr.getTimer(prefix,  managerMetricsName, M_MUTATE, M_TIME).time();
+
+        try {
+            backend.mutateMany(mutations,txh);
+        } catch (BackendException e) {
+            mgr.getCounter(prefix,  managerMetricsName, M_MUTATE, M_EXCEPTIONS).inc();
+            throw e;
+        } catch (RuntimeException e) {
+            mgr.getCounter(prefix,  managerMetricsName, M_MUTATE, M_EXCEPTIONS).inc();
+            throw e;
+        } finally {
+            tc.stop();
+        }
+    }
+
+    @Override
+    public StoreTransaction beginTransaction(BaseTransactionConfig config) throws BackendException {
+        MetricManager.INSTANCE.getCounter(GLOBAL_PREFIX, managerMetricsName, M_START_TX, M_CALLS).inc();
+        return backend.beginTransaction(config);
+    }
+
+    @Override
+    public void close() throws BackendException {
+        backend.close();
+        MetricManager.INSTANCE.getCounter(GLOBAL_PREFIX, managerMetricsName, M_CLOSE_MANAGER, M_CALLS).inc();
+    }
+
+    @Override
+    public void clearStorage() throws BackendException {
+        backend.clearStorage();
+    }
+
+    @Override
+    public StoreFeatures getFeatures() {
+        return backend.getFeatures();
+    }
+
+    @Override
+    public String getName() {
+        return backend.getName();
+    }
+
+    @Override
+    public List<KeyRange> getLocalKeyPartition() throws BackendException {
+        return backend.getLocalKeyPartition();
+    }
+}

titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/Backend.java
@@ -29,6 +29,7 @@ import com.thinkaurelius.titan.diskstorage.log.kcvs.KCVSLogManager;
 import com.thinkaurelius.titan.diskstorage.util.BackendOperation;
 import com.thinkaurelius.titan.diskstorage.util.MetricInstrumentedStore;
 import com.thinkaurelius.titan.diskstorage.configuration.backend.KCVSConfiguration;
+import com.thinkaurelius.titan.diskstorage.util.MetricInstrumentedStoreManager;
 import com.thinkaurelius.titan.diskstorage.util.StandardBaseTransactionConfig;
 import com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration;
 import com.thinkaurelius.titan.graphdb.configuration.TitanConstants;
@@ -74,6 +75,7 @@ public class Backend implements LockerProvider {
 
     public static final String ID_STORE_NAME = "titan_ids";
 
+    public static final String METRICS_STOREMANAGER_NAME = "storeManager";
     public static final String METRICS_MERGED_STORE = "stores";
     public static final String METRICS_MERGED_CACHE = "caches";
     public static final String METRICS_CACHE_SUFFIX = ".cache";
@@ -128,7 +130,12 @@ public class Backend implements LockerProvider {
     public Backend(Configuration configuration) {
         this.configuration = configuration;
 
-        storeManager = getStorageManager(configuration);
+        KeyColumnValueStoreManager manager = getStorageManager(configuration);
+        if (configuration.get(BASIC_METRICS)) {
+            storeManager = new MetricInstrumentedStoreManager(manager,METRICS_STOREMANAGER_NAME,configuration.get(METRICS_MERGE_STORES),METRICS_MERGED_STORE);
+        } else {
+            storeManager = manager;
+        }
         indexes = getIndexes(configuration);
         storeFeatures = storeManager.getFeatures();
 
@@ -206,13 +213,9 @@ public class Backend implements LockerProvider {
      */
     public void initialize(Configuration config) {
         try {
-            boolean reportMetrics = configuration.get(BASIC_METRICS);
-
             //EdgeStore & VertexIndexStore
             KeyColumnValueStore idStore = storeManager.openDatabase(ID_STORE_NAME);
-            if (reportMetrics) {
-                idStore = new MetricInstrumentedStore(idStore, getMetricsStoreName(ID_STORE_NAME));
-            }
+
             idAuthority = null;
             if (storeFeatures.isKeyConsistent()) {
                 idAuthority = new ConsistentKeyIDAuthority(idStore, storeManager, config);
@@ -222,11 +225,6 @@ public class Backend implements LockerProvider {
 
             KeyColumnValueStore edgeStoreRaw = storeManagerLocking.openDatabase(EDGESTORE_NAME);
             KeyColumnValueStore indexStoreRaw = storeManagerLocking.openDatabase(INDEXSTORE_NAME);
-
-            if (reportMetrics) {
-                edgeStoreRaw = new MetricInstrumentedStore(edgeStoreRaw, getMetricsStoreName(EDGESTORE_NAME));
-                indexStoreRaw = new MetricInstrumentedStore(indexStoreRaw, getMetricsStoreName(INDEXSTORE_NAME));
-            }
 
             //Configure caches
             if (cacheEnabled) {
@@ -251,8 +249,8 @@ public class Backend implements LockerProvider {
                 long edgeStoreCacheSize = Math.round(cacheSizeBytes * EDGESTORE_CACHE_PERCENT);
                 long indexStoreCacheSize = Math.round(cacheSizeBytes * INDEXSTORE_CACHE_PERCENT);
 
-                edgeStore = new ExpirationKCVSCache(edgeStoreRaw,getMetricsCacheName("edgeStore",reportMetrics),expirationTime,cleanWaitTime,edgeStoreCacheSize);
-                indexStore = new ExpirationKCVSCache(indexStoreRaw,getMetricsCacheName("indexStore",reportMetrics),expirationTime,cleanWaitTime,indexStoreCacheSize);
+                edgeStore = new ExpirationKCVSCache(edgeStoreRaw,getMetricsCacheName("edgeStore"),expirationTime,cleanWaitTime,edgeStoreCacheSize);
+                indexStore = new ExpirationKCVSCache(indexStoreRaw,getMetricsCacheName("indexStore"),expirationTime,cleanWaitTime,indexStoreCacheSize);
             } else {
                 edgeStore = new NoKCVSCache(edgeStoreRaw);
                 indexStore = new NoKCVSCache(indexStoreRaw);
@@ -331,12 +329,8 @@ public class Backend implements LockerProvider {
         return systemConfig;
     }
 
-    private String getMetricsStoreName(String storeName) {
-        return configuration.get(METRICS_MERGE_STORES) ? METRICS_MERGED_STORE : storeName;
-    }
-
-    private String getMetricsCacheName(String storeName, boolean reportMetrics) {
-        if (!reportMetrics) return null;
+    private String getMetricsCacheName(String storeName) {
+        if (!configuration.get(BASIC_METRICS)) return null;
         return configuration.get(METRICS_MERGE_STORES) ? METRICS_MERGED_CACHE : storeName + METRICS_CACHE_SUFFIX;
     }
 

titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanOperationCountingTest.java
@@ -202,9 +202,32 @@ public abstract class TitanOperationCountingTest extends TitanGraphBaseTest {
 
     }
 
-
     public static final List<String> STORE_NAMES =
-            ImmutableList.of("edgeStore", "vertexIndexStore", "edgeIndexStore", "idStore");
+            ImmutableList.of(Backend.EDGESTORE_NAME,Backend.INDEXSTORE_NAME,Backend.ID_STORE_NAME,Backend.METRICS_STOREMANAGER_NAME);
+
+    @Test
+    public void testSettingProperty() throws Exception {
+        metricsPrefix = "metrics1";
+
+        mgmt.makePropertyKey("foo").dataType(String.class).cardinality(Cardinality.SINGLE).make();
+        finishSchema();
+
+        TitanVertex v = tx.addVertex();
+        v.setProperty("foo","bar");
+        tx.commit();
+
+
+        TitanTransaction tx = graph.buildTransaction().checkExternalVertexExistence(false).setGroupName(metricsPrefix).start();
+        v = tx.getVertex(v.getLongId());
+        v.setProperty("foo","bus");
+        tx.commit();
+        printAllMetrics();
+        verifyStoreMetrics(STORE_NAMES.get(0));
+        verifyStoreMetrics(STORE_NAMES.get(1));
+        verifyStoreMetrics(STORE_NAMES.get(2));
+        verifyStoreMetrics(STORE_NAMES.get(3), ImmutableMap.of(M_MUTATE, 1l));
+    }
+
 
     @Test
     @Ignore //TODO: Ignore for now until everything is stable - then do the counting

commit d78a0801de473a1943061ef616d0252b55b98f66

Fix HBase + TTL + Metrics
278d8b988058183ed032b03959771864f6b37a0c introduced an exception
during graph startup when configured to use HBase with Metrics.  This
causes setup errors in HBaseOperationCountingTest, but it's not just
an issue of updating some outdated expectation numbers as in
ee8f75a604071db919f0a18a11a1bc72cc1c883f.  This is actually a problem
with Titan proper.

The commit that introduced this regression wraps the backend with
MetricsInstrumentedStoreManager early in startup, before wrapping with
TTLKCVSManager.  TTLKCVSManager expects `instanceof
CustomizeKCVSManager` on its encapsulated backend to be true unless it
implements cell-level TTL; this is true of HBaseSM, but not of
MetricsInstrumentedSM.  Doing instanceof <some manager-extension
interface> in wrappers is generally problematic since we have so many
manager/store wrapper classes.  The typecasts prevent the compiler
from helping us catch this stuff.  However, this is (hopefully) the
last commit before the next bugfix release, so I don't want to go
overboard with hurried refactoring and break something else.  I made
TTLKCVSManager implement CustomizeStoreKCVSManager and check that its
wrapped backend is instanceof same when the openDatabase(name, ttl)
method is invoked.

titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/util/MetricInstrumentedStoreManager.java
@@ -17,7 +17,7 @@ import static com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfigu
 /**
  * @author Matthias Broecheler (me@matthiasb.com)
  */
-public class MetricInstrumentedStoreManager implements KeyColumnValueStoreManager {
+public class MetricInstrumentedStoreManager implements CustomizeStoreKCVSManager {
 
     public static final String M_OPEN_DATABASE = "openDatabase";
     public static final String M_START_TX = "startTransaction";
@@ -48,6 +48,13 @@ public class MetricInstrumentedStoreManager implements KeyColumnValueStoreManage
     public KeyColumnValueStore openDatabase(String name) throws BackendException {
         MetricManager.INSTANCE.getCounter(GLOBAL_PREFIX, managerMetricsName, M_OPEN_DATABASE, M_CALLS).inc();
         return new MetricInstrumentedStore(backend.openDatabase(name),getMetricsStoreName(name));
+    }
+
+    @Override
+    public KeyColumnValueStore openDatabase(String name, int ttlInSeconds) throws BackendException {
+        Preconditions.checkArgument(backend instanceof CustomizeStoreKCVSManager,
+                "StoreManager %s does not support store-level TTL (not instanceof CustomizeKCVSManager)", backend);
+        return ((CustomizeStoreKCVSManager)backend).openDatabase(name, ttlInSeconds);
     }
 
     @Override